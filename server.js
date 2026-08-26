f5303db3712818587302fd90999f4a85edb47f00
abhinandant-07 <tabhinandan2007@gmail.com>
2026-08-26 15:49:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const multer = require("multer");
 const db = require("./database");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
